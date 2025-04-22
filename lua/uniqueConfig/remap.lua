cbd4f9f5dd3a92ef8826fd14c498d5e5e80bb4db
MichaelKershaw <MichaelKershaw2006@gmail.com>
2025-04-22 14:57:38 +1000

    added split window stuff and reordered harpoon keybinds

--- lua/uniqueConfig/remap.lua
+++ lua/uniqueConfig/remap.lua
@@ -14,13 +14,15 @@ for _, key in ipairs(keysDown) do
   vim.keymap.set("n", key, "V:m '>+1<CR>gv=gv<C-c>")
 end
 
-vim.keymap.set("n", "wl", function() 
+vim.keymap.set("n", "<leader>wl", function() 
   vim.opt.wrap = not vim.opt.wrap:get()
 end)
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-Down>", "<C-d>zz")
+vim.keymap.set("n", "<C-Up>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -42,4 +44,8 @@ vim.keymap.set("n", "<S-Tab>", "V<")
 vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
 vim.keymap.set("v", "<C-_>", "gcgv", { remap = true })
 
-vim.keymap.set("n", "<leader><leader>", ":so<Enter>")
+vim.keymap.set("n", "<leader><leader>", ":so<CR>")
+
+vim.keymap.set("n", "<M-v>", ":vsplit<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<M-h>", ":split<CR>", { noremap = true, silent = true })

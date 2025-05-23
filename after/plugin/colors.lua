3682f5e2e9ad531ae31b98ffdf0719e550c33e6c
Michael Kershaw <michaelkershaw2006@gmail.com>
2025-05-23 20:35:49 +1000

    changed background to black because that just looked best to me

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,8 +6,8 @@ function ColorMyPencils(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
 
-  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
 
 end
 

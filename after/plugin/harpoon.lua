31ea0adde6caf5e1adb34b40a633841a860ed045
MichaelKershaw <MichaelKershaw2006@gmail.com>
2025-09-29 19:48:31 +1000

    i could of sworn that i already fixed this but harpoon bindings now are all uniuqe

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,6 +8,6 @@ vim.keymap.set("n", "<C-m>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(5) end)
 
 vim.keymap.set('n', '<leader>f', function() ui.nav_file(tonumber(vim.fn.input("File Index > "))) end)
